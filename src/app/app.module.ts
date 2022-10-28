10f7bad481fb223db4d212839d4b55817889a319
altynbekM01 <altynbek.01m@gmail.com>
2022-10-28 22:35:30 +0600

    commit5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,26 @@ import 'hammerjs';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -26,9 +41,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatButtonModule,
     MatGridListModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    AppRoutingModule
+
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,33 +1,35 @@
 import {Component, OnInit} from '@angular/core';
 import {Dish} from  '../shared/dish';
+import {DISHES} from "../shared/dishes";
+import { DishService } from '../services/dish.service';
 
 
-const DISHES: Dish[] = [
-  {id: '0',
-    name: 'Uthapizza',
-    image: 'assets/images/uthappizza.png',
-    category: 'mains',
-    featured: true,
-    label: 'Hot',
-    price: '4.99',
-    description: 'Delicious',
-  },
-  {id: '1',
-    name: 'Zuchipakoda',
-    image: 'assets/images/zucchipakoda.png',
-    category: 'appetizer',
-    featured: false,
-    label: '',
-    price: '1.99',
-    description: 'Delicious',
-  }
-
-];
+// const DISHES: Dish[] = [
+//   {id: '0',
+//     name: 'Uthapizza',
+//     image: 'assets/images/uthappizza.png',
+//     category: 'mains',
+//     featured: true,
+//     label: 'Hot',
+//     price: '4.99',
+//     description: 'Delicious',
+//   },
+//   {id: '1',
+//     name: 'Zuchipakoda',
+//     image: 'assets/images/zucchipakoda.png',
+//     category: 'appetizer',
+//     featured: false,
+//     label: '',
+//     price: '1.99',
+//     description: 'Delicious',
+//   }
+//
+// ];
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.scss']
 })
 
 
@@ -39,16 +41,16 @@ const DISHES: Dish[] = [
 
 export class MenuComponent implements OnInit {
 
-  dishes = DISHES;
-  selectedDish = DISHES[0];
-
-
-
-  constructor() {
+ dishes!: Dish[];
+ selectedDish!: Dish;
+  onSelect(dish: Dish){
+    this.selectedDish = dish;
   }
+ constructor(private dishService: DishService) { }
 
-  ngOnInit(): void {
-  }
+ ngOnInit() {
+ this.dishes = this.dishService.getDishes();
+ }
 
 
 
